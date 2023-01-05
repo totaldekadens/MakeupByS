6e68093adc7d4c1fcb936376d42d314eb999305c
totaldekadens <angelica.moberg@hotmail.com>
2023-01-05 17:55:59 +0100

    initial totalsum

--- components/checkout/TotalSum.tsx
+++ components/checkout/TotalSum.tsx
@@ -1,10 +1,18 @@
-import { Flex, Title } from "@mantine/core";
+import { Flex, Title, Text, Button } from "@mantine/core";
 import { FC } from "react";
 
 const TotalSum: FC = () => {
   return (
-    <Flex>
-      <Title>Heeej</Title>
+    <Flex mt={20} direction={"column"} align="center">
+      <Text weight={"bold"}>Att betala inkl. moms och frakt:</Text>
+      <Title color={"red"}>1000 KR</Title>
+      <Button sx={{ width: "100%" }} mt={40}>
+        Gå till betalning
+      </Button>
+      <Text mt={5} size={"xs"}>
+        Genom att fortsätta till betalning godkänner jag Almänna villkor, samt
+        bekräftar att jag tagit del av, och förstått butikens integritetpolicy
+      </Text>
     </Flex>
   );
 };
