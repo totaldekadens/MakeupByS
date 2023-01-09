9bcd094ef16ea638f049a939eb0974e8e9990818
totaldekadens <angelica.moberg@hotmail.com>
2023-01-09 11:34:12 +0100

    Address fixed

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,11 +1,13 @@
-import { AppShell, Flex, Title, Text } from "@mantine/core";
+import { AppShell, Flex, Title, Text, Box } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { LineItem } from "../components/AddToCartIcon";
 import { Checkout } from "../components/context/checkoutProvider";
 import HeaderCheckout from "../components/HeaderCheckout";
+import ContainerWithBorder from "../components/layout/ContainerWithBorder";
+import { OrderDocument } from "../models/Order";
 
 const SuccessPage: NextPage = (props) => {
   // Local storage
@@ -20,6 +22,9 @@ const SuccessPage: NextPage = (props) => {
     key: "cart",
     defaultValue: [],
   });
+
+  // State
+  const [orderConfirmation, setOrderConfirmation] = useState<OrderDocument>();
 
   // Router
   const router = useRouter();
@@ -61,6 +66,9 @@ const SuccessPage: NextPage = (props) => {
         body,
       });
       let result = await response.json();
+      if (result.success) {
+        setOrderConfirmation(result.data);
+      }
       return result;
     }
   };
@@ -87,7 +95,7 @@ const SuccessPage: NextPage = (props) => {
         removeCheckoutLocal();
         removeCartItems();
       } else if (response.status == 400) {
-        router.push("/");
+        //router.push("/");    // Borttagen bara sålänge jag håller på med ordebekräftelsen.
       } else {
         router.push(`/kassa?message=${result.data}`);
       }
@@ -102,7 +110,131 @@ const SuccessPage: NextPage = (props) => {
         direction="column"
         style={{ marginTop: 60, width: "100%" }}
       >
-        <Title>Success!!</Title>
+        {orderConfirmation ? (
+          <>
+            <Title order={1}>Orderbekräftelse</Title>
+            <ContainerWithBorder>
+              <Flex gap={10} align="center" mb={10}>
+                <Title order={3}> Order: </Title>
+                <Text weight={"bold"}>{orderConfirmation.orderNo}</Text>
+              </Flex>
+
+              {orderConfirmation.deliveryAddress ? (
+                <>
+                  <Box mb={20}>
+                    <Title order={4}>Beställare: </Title>
+                    <Flex direction={"column"}>
+                      <Flex direction="column">
+                        <Text>{orderConfirmation.email}</Text>
+                        <Text>{orderConfirmation.invoiceAddress.line1},</Text>
+                        <Text>
+                          {orderConfirmation.invoiceAddress?.line2
+                            ? orderConfirmation.invoiceAddress?.line2 + ","
+                            : null}
+                        </Text>
+
+                        <Flex gap={7}>
+                          <Text>
+                            {orderConfirmation.invoiceAddress?.postal_code}
+                          </Text>
+                          <Text>{orderConfirmation.invoiceAddress?.city}</Text>
+                        </Flex>
+                      </Flex>
+                      <Flex
+                        gap={3}
+                        sx={(theme) => ({
+                          [theme.fn.smallerThan("xs")]: {
+                            flexDirection: "column",
+                          },
+                        })}
+                      ></Flex>
+                    </Flex>
+                  </Box>
+                  <Box>
+                    <Title order={4}>Levereras till: </Title>
+                    <Flex direction={"column"}>
+                      <Text>{orderConfirmation.name}</Text>
+                      <Flex
+                        gap={3}
+                        sx={(theme) => ({
+                          [theme.fn.smallerThan("xs")]: {
+                            flexDirection: "column",
+                          },
+                        })}
+                      >
+                        <Text>{orderConfirmation.deliveryAddress?.line1},</Text>
+                        <Text>
+                          {orderConfirmation.deliveryAddress?.line2
+                            ? orderConfirmation.deliveryAddress?.line2 + ","
+                            : null}
+                        </Text>
+
+                        <Flex gap={7}>
+                          <Text>
+                            {orderConfirmation.deliveryAddress?.postal_code}
+                          </Text>
+                          <Text>
+                            {orderConfirmation.deliveryAddress?.city},
+                          </Text>
+                        </Flex>
+                      </Flex>
+                      <Flex
+                        gap={3}
+                        sx={(theme) => ({
+                          [theme.fn.smallerThan("xs")]: {
+                            flexDirection: "column",
+                          },
+                        })}
+                      >
+                        <Text>{orderConfirmation.phone}</Text>
+                      </Flex>
+                    </Flex>
+                  </Box>
+                </>
+              ) : (
+                <Box>
+                  <Title order={4}>Levereras till: </Title>
+                  <Flex direction={"column"}>
+                    <Text>{orderConfirmation.name}</Text>
+                    <Flex
+                      gap={3}
+                      sx={(theme) => ({
+                        [theme.fn.smallerThan("xs")]: {
+                          flexDirection: "column",
+                        },
+                      })}
+                    >
+                      <Text>{orderConfirmation.invoiceAddress.line1},</Text>
+                      <Text>
+                        {orderConfirmation.invoiceAddress.line2
+                          ? orderConfirmation.invoiceAddress.line2 + ","
+                          : null}
+                      </Text>
+
+                      <Flex gap={7}>
+                        <Text>
+                          {orderConfirmation.invoiceAddress.postal_code}
+                        </Text>
+                        <Text>{orderConfirmation.invoiceAddress.city},</Text>
+                      </Flex>
+                    </Flex>
+                    <Flex
+                      gap={3}
+                      sx={(theme) => ({
+                        [theme.fn.smallerThan("xs")]: {
+                          flexDirection: "column",
+                        },
+                      })}
+                    >
+                      <Text>{orderConfirmation.email}, </Text>
+                      <Text>{orderConfirmation.phone}</Text>
+                    </Flex>
+                  </Flex>
+                </Box>
+              )}
+            </ContainerWithBorder>
+          </>
+        ) : null}
       </Flex>
     </AppShell>
   );
