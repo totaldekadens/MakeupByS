1e6a1106d62d81c2d994613af8fe0b997bd6a7ae
totaldekadens <angelica.moberg@hotmail.com>
2023-01-12 10:34:00 +0100

    Kontent till sidorna klara

--- pages/integritet.tsx
+++ pages/integritet.tsx
@@ -1,20 +1,290 @@
 import { AppShell, Flex, Title, Text } from "@mantine/core";
 import { NextPage } from "next";
 import Header from "../components/Header";
+import ContainerWithBorder from "../components/layout/ContainerWithBorder";
 import MarginTopContainer from "../components/layout/MarginTopContainer";
+import { Br, Ti } from "./kopvillkor";
 
 const Integritet: NextPage = (props) => {
   return (
     <AppShell fixed={false} header={<Header />}>
       <MarginTopContainer>
-        <Flex
-          align="center"
-          direction="column"
-          style={{ marginTop: 60, width: "100%" }}
-        >
-          <Title>Integritetspolicy</Title>
-          <Text>text</Text>
-        </Flex>
+        <ContainerWithBorder>
+          <Flex direction="column" style={{ width: "100%" }}>
+            <Title align="center">Integritetspolicy</Title>
+            <Text size={14} mt={20}>
+              Din personliga integritet är viktig för MakeUpByS AB. I denna
+              policy vill vi informera dig om hur vi behandlar dina
+              personuppgifter och rättigheter. Personuppgifter är information
+              som kan bli sammankopplad med dig som privatperson, till exempel
+              ditt namn och din kontaktinformation.
+            </Text>
+            <Ti>För att summera, behandlar vi dina personuppgifter:</Ti>
+            <Br>
+              <li>
+                I den utsträckning som krävs för att kunna hantera och leverera
+                din order.
+              </li>
+            </Br>
+            <Br>
+              <li>
+                För att ge erbjudanden och inspiration till dig. Både genom
+                nyhetsbrev, sms och riktad personanpassad marknadsföring i
+                sociala medier, samt i övrigt online och offline.
+              </li>
+            </Br>
+            <Br>
+              <li>För att kommunicera med dig och besvara dina frågor.</li>
+            </Br>
+
+            <Br>
+              <li>För att fråga dig om MakeUpByS och din upplevelse av oss.</li>
+            </Br>
+            <Br>
+              <li>För att påminna dig om en övergiven ”shoppingkorg”.</li>
+            </Br>
+            <Br>
+              <li>För att skapa och underhålla ett kundkonto.</li>
+            </Br>
+            <Br>
+              <li>
+                För att följa lagar och regler (t.ex. konsument- och
+                redovisningsrätt).
+              </li>
+            </Br>
+            <Br>
+              Du har flera rättigheter enligt dataskyddslagen. Till exempel har
+              du alltid rätt att invända mot marknadsföring. Du kan också läsa
+              om hur du kan påverka vår behandling av dina personuppgifter –
+              vilka rättigheter du har.
+            </Br>
+            <br />
+            <Br>
+              Vill du veta mer? Läs gärna våra detaljerade förklaringar nedan.
+              Du kan läsa om hur, varför och hur lång tidsperiod vi behandlar
+              dina personuppgifter.
+            </Br>
+            <br />
+            <Ti>Vem är ansvarig för behandlingen av dina personuppgifter?</Ti>
+            <Br>
+              MakeUpByS AB, med organisationsnummer 559153-2493, är ansvariga
+              för behandlingen av dina personuppgifter. Notera att våra
+              leverantörer av betalningslösningar, till exempel Qliro, behandlar
+              personuppgifter som de hämtar från vår hemsida vid genomförande av
+              köp och är själva ansvariga för deras behandling. Om du vill
+              kontakta oss om behandlingen av dina personuppgifter, kontakta oss
+              på info@makeupbys.se. Vår adress är Biblioteksgatan 29, 6 vån, 114
+              35 Stockholm.
+            </Br>
+            <Ti>Från vem eller var hämtar vi dina personuppgifter?</Ti>
+            <Br>Vi behandlar personuppgifter som du har delat med oss.</Br>
+            <Ti>Vem kan få tillgång till dina personuppgifter?</Ti>
+            <Br>
+              Dina personuppgifter behandlas huvudsakligen av oss på MakeUpByS.
+              Vi kommer aldrig att sälja dina personuppgifter. I vissa fall
+              delar vi dina personuppgifter för att kunna fullfölja våra
+              skyldigheter till dig på ett bra och effektivt sätt:
+            </Br>
+            <Br>
+              <li>
+                För att vi försäkra oss om att du betalar oss så kommer våra
+                leverantörer av betalningslösningar få tillgång till dina
+                personuppgifter.
+              </li>
+            </Br>
+            <Br>
+              <li>
+                För att vi ska kunna leverera dina produkter till din hemadress
+                eller utlämningsställe och för att hantera returer så kommer vi
+                dela dina personuppgifter med fraktbolag.
+              </li>
+            </Br>
+            <Br>
+              <li>
+                För att inhämta och publicera dina recensioner på vår hemsida
+                delar vi dina personuppgifter med leverantören som förser oss
+                med tekniska lösningar för att inhämta och publicera
+                recensionerna.
+              </li>
+            </Br>
+            <Br>
+              <li>
+                För att marknadsföra relevanta produkter och för att göra vår
+                hemsida så relevant som möjligt delar vi dina personuppgifter
+                med de som förser oss marknadsföringstjänster. Exempelvis Google
+                och Facebook.
+              </li>
+            </Br>
+            <Br>
+              <li>
+                Vi kommer dela dina personuppgifter med IT-leverantörer som
+                behandlar personuppgifterna på uppdrag av oss som biträden för
+                att hjälpa oss med IT-tjänster.
+              </li>
+            </Br>
+            <Br>
+              Du kan kontakta oss ifall du vill ha mer detaljerad information om
+              vilka vi delar dina personuppgifter med.
+            </Br>
+            <Ti>Överför vi dina personuppgifter utanför EU/EEA?</Ti>
+            <Br>
+              MakeUpByS behandlar generellt dina personuppgifter inom EU/EEA men
+              i enskilda fall använder vi leverantörer från länder utanför
+              EU/EEA. När vi överför dina personuppgifter utanför EU/EEA
+              säkerställer MakeUpByS att det finns en tillräckligt hög
+              skyddsnivå. MakeUpByS kan komma att överföra dina personuppgifter
+              till USA, till vår nyhetsbrevsleverantör samt våra leverantörer
+              som tillhandahåller tjänster för att marknadsföra relevanta
+              produkter och för att göra vår hemsida så relevant som möjligt.
+              Kontakta oss ifall du har några frågor om hur vi delar dina
+              personuppgifter.
+            </Br>
+            <Ti>Hur kan du påverka behandlingen av dina personuppgifter?</Ti>
+            <Br>
+              Enligt dataskyddslagar har du flera rättigheter att påverka vår
+              behandling av dina personuppgifter. Läs mer nedan.
+            </Br>
+            <Ti>Rätt att återkalla samtycke och att invända mot behandling</Ti>
+            <Br>
+              Du har rätt att helt eller delvis återkalla samtycke för
+              behandlingen av dina personuppgifter. Din återkallelse kommer
+              gälla från och med tidpunkten då du återkallar samtycket. Du har
+              rätt att invända mot behandlingen av dina personuppgifter. Du har
+              alltid rätt att invända mot marknadsföring från oss, som till
+              exempel nyhetsbrev. Du har generellt sett rätt att invända när
+              behandlingen baseras på intresseavvägning. Läs mer om vad det
+              betyder nedan. I vissa andra fall existerar inte rätten att
+              invända (t.ex. eftersom vi måste spara dina personuppgifter). Om
+              vi kan visa övertygande legala skäl för behandlingen som väger
+              tyngre än dina intressen och grundläggande rättigheter eller om
+              det är för anledningen att avgöra, utöva eller försvara rättsliga
+              krav har du aldrig rätt att göra invändningar mot behandlingen.
+            </Br>
+            <Ti>Rätt till tillgång</Ti>
+            <Br>
+              Du har rätt att få bekräftelse på om vi behandlar personuppgifter
+              som berör dig. Du kan få tillgång till personuppgifterna och få en
+              kopia av personuppgifterna som vi behandlar.
+            </Br>
+            <Ti>Rätt till rättelse</Ti>
+            <Br>
+              Du har rätt att få rättning av felaktiga personuppgifter som berör
+              dig och att be oss att få ofullständiga personuppgifter
+              kompletterade.
+            </Br>
+            <Ti>
+              Rätt till radering (”rätten att bli glömd”) och begränsning av
+              behandling
+            </Ti>
+            <Br>
+              Under vissa omständigheter har du rätt till radering av dina
+              personuppgifter. Detta gäller exempelvis om personuppgifterna inte
+              längre är nödvändiga för syftet som de samlades in eller
+              behandlades eller då du återkallar ditt samtycke som behandlingen
+              baseras på och där det inte finns någon annan laglig grund för att
+              fortsätta behandlingen. Du har också rätt att begära att MakeUpByS
+              begränsar behandlingen av dina personuppgifter. Detta gäller
+              exempelvis när personuppgifternas korrekthet ifrågasätts av dig
+              eller om behandlingen är olaglig och du motsätter dig raderingen
+              av personuppgifterna och istället begär att användningen
+              begränsas.
+            </Br>
+            <Ti>Rätt att lämna in klagomål till en tillsynsmyndighet</Ti>
+            <Br>
+              Du har alltid rätt till att lämna in klagomål till en
+              tillsynsmyndighet. Du kan göra det i den särskilda
+              EU/EEA-medlemsstat där du har din hemvist, där du jobbar eller där
+              en påstådd överträdelse av gällande lagstiftning om dataskydd
+              skett. I Sverige är Datainspektionen tillsynsmyndigheten. Denna
+              rätt påverkar inte andra administrativa eller rättsliga åtgärder.
+            </Br>
+            <Ti>Rätt till dataportabilitet</Ti>
+            <Br>
+              Du har rätt att fråga oss att överföra vissa av dina
+              personuppgifter som vi har om dig till ett annat företag
+              (dataportabilitet). I denna rätt tillämpas personuppgifter du har
+              försett oss med i ett strukturerat, vanligen använt, maskinläsbart
+              och kompatibelt format ifall:
+            </Br>
+            <Br>
+              <li>
+                Behandlingen är baserad på samtycke eller ett kontrakt; och
+              </li>
+            </Br>
+            <Br>
+              <li>Behandlingen utförs automatiskt</li>
+            </Br>
+            <Br>
+              När du utövar din rättighet till dataportabilitet har du, där det
+              är tekniskt möjligt, rätt att få personuppgifter överförda direkt
+              från MakeUpByS till en annan dataansvarig.
+            </Br>
+            <Ti>Hur och varför behandlar vi dina personuppgifter?</Ti>
+            <Br>
+              Vårt mål är att vara så transparenta som möjligt angående hur och
+              varför vi behandlar dina personuppgifter. I tabellen kan du läsa
+              mer detaljerad information om varför vi behandlar dina
+              personuppgifter (syften med behandlingen), vilka personuppgifter
+              vi behandlar, vår rättsliga grund för behandling av dina
+              personuppgifter och hur länge vi behandlar dina personuppgifter
+              för varje syfte.
+            </Br>
+            <Ti>Behandling före köp</Ti>
+            <Br>
+              För att vi ska kunna kommunicera med dig och för att få feedback
+              från dig så måste vi behandla dina personuppgifter. Om du inte
+              förser oss med dina personuppgifter för dessa syften så kommer vi
+              inte kunna förse dig med tjänsten eller besvara dina frågor.
+            </Br>
+            <Ti>
+              Behandling som krävs för genomförande av köp och avtal, etc.
+            </Ti>
+            <Br>
+              För att vi ska kunna genomföra ditt köp, till exempel leverera
+              produkten, behöver vi behandla dina personuppgifter. Vi behöver
+              också behandla dina personuppgifter för att följa lagstadgar eller
+              andra krav, som till exempel bokföringslagens krav att spara
+              uppgifter samt konsumentskyddslagar. Om du inte förser oss med
+              personuppgifter för dessa syften kommer vi inte kunna genomföra
+              ditt köp hos oss. Vänligen notera att även våra leverantörer av
+              betalningslösningar behandlar dina personuppgifter för att
+              administrera betalningen för din order. Våra leverantörer av
+              betalningslösningar är oberoende ansvariga för sån behandling.
+              Vissa av våra leverantörer av betalningslösningar tillåter dig att
+              välja ett enklare sätt att genomföra köp genom att automatiskt
+              fylla i dina personuppgifter eller bli ihågkommen på din enhet.
+            </Br>
+            <Ti>Behandling för att hantera ditt kundkonto</Ti>
+            <Br>
+              För att hantera ditt kundkonto behandlar vi dina personuppgifter
+              för utförandet av vårt kontrakt med dig. Om du inte förser oss med
+              personuppgifter för detta syfte kan vi inte förse dig med ditt
+              kundkonto.
+            </Br>
+            <Ti>
+              Hur har vi bedömt intresseavvagägningen när den rättsliga grunden
+              för behandling av dina personlighetsuppgifterär vårt berättigade
+              intresse?
+            </Ti>
+            <Br>
+              För vissa ändamål behandlar MakeUpByS AB dina personuppgifter och
+              förlitar oss på vårt berättigade intresse som rättslig grund för
+              behandlingen. Vid bedömning av den rättsliga grunden förlitar vi
+              oss på ett intresseavvägningstest genom vilket vi har fastställt
+              att våra legitima intressen för behandlingen väger tyngre än ditt
+              intresse och din grundläggande rättighet att inte få dina
+              personuppgifter behandlade. Vi har angivit vårt berättigade
+              intresse i tabellerna ovan. Vänligen kontakta oss ifall du vill
+              läsa mer om hur vi har gjort detta test. Våra kontaktuppgifter
+              anges i början av denna integritetspolicy.
+            </Br>
+            <br />
+            <Br>
+              Denna integritetspolicy uppdaterades av MakeUpByS AB den 8 januari
+              2023.
+            </Br>
+          </Flex>
+        </ContainerWithBorder>
       </MarginTopContainer>
     </AppShell>
   );
--- pages/kopvillkor.tsx
+++ pages/kopvillkor.tsx
@@ -1,20 +1,188 @@
 import { AppShell, Flex, Title, Text } from "@mantine/core";
 import { NextPage } from "next";
+import { FC, PropsWithChildren } from "react";
 import Header from "../components/Header";
+import ContainerWithBorder from "../components/layout/ContainerWithBorder";
 import MarginTopContainer from "../components/layout/MarginTopContainer";
+
+export const Ti: FC<PropsWithChildren> = ({ children }) => (
+  <Text size={18} mt={15}>
+    {children}
+  </Text>
+);
+
+export const Br: FC<PropsWithChildren> = ({ children }) => (
+  <Text size={14} mt={8}>
+    {children}
+  </Text>
+);
 
 const Kopvillkor: NextPage = (props) => {
   return (
     <AppShell fixed={false} header={<Header />}>
       <MarginTopContainer>
-        <Flex
-          align="center"
-          direction="column"
-          style={{ marginTop: 60, width: "100%" }}
-        >
-          <Title>Köpvillkor</Title>
-          <Text>text</Text>
-        </Flex>
+        <ContainerWithBorder>
+          <Flex direction="column" style={{ width: "100%" }}>
+            <Title mb={20} align="center">
+              Köpvillkor
+            </Title>
+            <Ti>Allmänt</Ti>
+            <Br>
+              Samtliga priser inkluderar vid försäljningstillfället gällande
+              mervärdesskatt, för närvarande 25%. Frakt tillkommer på alla
+              priser om inte annat anges. För att genomföra ett köp på
+              makeupbys.se krävs det att du är över 18 år. Är du under 18 år
+              krävs enligt Föräldrabalken målsmans medgivande. Genom beställning
+              på makeupbys.se godkänner du de vid tiden publicerade villkor som
+              anges nedan och intygar att de uppgifter du lämnar om dig själv är
+              korrekta.
+            </Br>
+            <Text size={14} mt={5}>
+              Med benämningarna vi, våra, vårt och oss nedan avses MakeUpByS AB.
+            </Text>
+            <Text mt={10} size={14} weight={"bold"}>
+              Företagsinformation
+            </Text>
+            <Br>
+              MakeUpByS AB <br /> Biblioteksgatan 29, 6 vån <br /> 114 35
+              Stockholm <br />
+              e-mail: info@makeupbys.se <br />
+              Organisationsnummer: 559153-2493
+            </Br>
+            <Ti>Ansvarsbegränsning</Ti>
+            <Br>
+              Vi tar inget ansvar för indirekta skador som kan uppstå på grund
+              av produkten. Vi accepterar inget ansvar för förseningar/fel till
+              följd av omständigheter utanför företagets rådande (Force
+              Majeure). Dessa omständigheter kan exempelvis vara arbetskonflikt,
+              eldsvåda, krig, myndighetsbeslut, förminskad eller utebliven
+              leverans från leverantör. Vidare tas inget ansvar för eventuella
+              förändringar på produkter/produktegenskaper som ändrats av
+              respektive leverantör och andra faktorer utanför vår kontroll.
+            </Br>
+            <Ti>Betalning</Ti>
+            <Br>Vi accepterar kredit- och bankkort som betalningsmetod.</Br>
+            <Ti>Ej uthämtade paket</Ti>
+            <Br>
+              Du måste uppge giltig och korrekt leveransinformation. Om
+              MakeUpByS AB får tillbaka ett paket som skickats till fel adress
+              eller inte hämtats ut är det upp till dig att betala ny frakt. För
+              beställningar som inte hämtas ut debiterar vi en avgift på 250
+              SEK.
+            </Br>
+            <Ti>Kampanjer</Ti>
+            <Br>
+              Det går inte att kombinera eventuella rabattkoder med övriga
+              kampanjer eller erbjudanden, om inget annat anges
+            </Br>
+            <Ti>Leverans</Ti>
+            <Br>
+              Våra normala leveranstider är 2-7 arbetsdagar beroende på valt
+              fraktalternativ. Beställningar lagda på helger skickas tidigast
+              måndagen efter. Om förseningar i leveransen skulle uppstå (utan
+              att vi har meddelat dig om längre leveranstid) ber vi dig kontakta
+              vår kundtjänst.
+            </Br>
+            <Ti>Priser</Ti>
+            <Br>
+              Alla priser på hemsidan anges i SEK och är inklusive moms (om
+              inget annat anges eller valts av användaren). Vi reserverar oss
+              för prisändringar beroende på felaktig information och förbehåller
+              oss rätten att justera priset.
+            </Br>
+            <Ti>Produktinformation</Ti>
+            <Br>
+              Vi reserverar oss för eventuella tryckfel på denna webbplats samt
+              slutförsäljning av produkter. Vi garanterar inte att bilderna
+              återger produkternas exakta utseende då en viss färgskillnad kan
+              förekomma beroende på bildskärm, fotokvalitet samt upplösning. Vi
+              försöker alltid på bästa sätt att exponera produkterna så korrekt
+              som möjligt.
+            </Br>
+            <Ti>Skada</Ti>
+            <Br>
+              I fall av trasig eller öppnad kartong vid leverans, kontakta ditt
+              utlämningsställe direkt när du hämtar ut paketet. De har färdiga
+              blanketter för skadeanmälan.
+            </Br>
+            <Ti>Tvist</Ti>
+            <Br>
+              I tillfälle av att tvist inte kan lösas i samförstånd med vår
+              kundtjänst och dig, kan du som kund vända dig till Allmänna
+              Reklamationsnämnden. Allmänna Reklamationsnämnden har
+              telefonnummer 08-508 860 00.
+            </Br>
+            <Ti>Utebliven orderbekräftelse</Ti>
+            <Br>
+              På grund av tekniskt fel eller andra omständigheter bortom
+              MakeUpByS Cosmetics kontroll kan din orderbekräftelse bli sen
+              eller helt utebli. Om din orderbekräftelse dröjer mer än 30
+              minuter ber vi dig kontakta vår kundtjänst.
+            </Br>
+            <Ti>Ändring av villkor</Ti>
+            <Br>
+              Vi förbehåller oss rätten att när som helst företa ändringar i
+              villkoren. Ändringar av villkoren kommer att publiceras online på
+              webbplatsen. De ändrade villkoren anses för accepterade i samband
+              med beställning eller besök på webbplatsen
+            </Br>
+            <Ti>Reklamation</Ti>
+            <Br>
+              Vi följer Konsumentverkets rekommendationer för e-handel.
+              Kundtjänst måste alltid kontaktas via vårt kontaktformulär, e-post
+              (info@makeupbys.se) inom skälig tid. För mer information se nedan.
+              Reklamationen måste göras via vårt kontaktformulär. Reklamation av
+              trasig produkt ska ske inom skälig tid efter det att felet har
+              upptäckts, dock senast inom 1 år från köptillfället. Observera att
+              den trasiga produkten samt emballaget måste sparas tills dess att
+              ärendet är löst. Om varan ej finns i lager eller har utgått så
+              kontaktar vi dig innan vidare hantering. Vi hanterar
+              reklamationsärenden normalt sett inom 3-14 arbetsdagar. Vid
+              godkänd reklamation av köp så återbetalar vi summan för din
+              beställning inom 30 dagar.
+            </Br>
+            <Ti>Undantag från ångerrätt</Ti>
+            <Br>
+              Produkten måste vara obruten och oanvänd men enligt lag har du
+              rätt att säkerställa dess egenskaper och funktioner. Vid nyttjande
+              av ångerrätten har vi rätt att göra ett värdeminskningsavdrag i de
+              fall då varan hanterats i större omfattning än nödvändigt.
+              Värdeminskning bedöms från fall till fall. Öppnade och/eller
+              använda makeup produkter återtas inte på grund utav hälso- och
+              hygienskäl. Vid godkänd hävning av köp så återbetalar vi summan
+              för din beställning inom 21 dagar.
+            </Br>
+            <Ti>Ångerrätt</Ti>
+            <Br>
+              Vid köp av makeup-produkter på makeupbys.se har du som kund 30
+              dagars ångerrätt som gäller från det att du har tagit emot en vara
+              som du har beställt. Du är ansvarig för att kontrollera att
+              leveransen och produkterna är oskadda i samband med mottagande av
+              paketet.
+            </Br>
+            <Br>
+              Du har 30 dagars öppet köp från det att du mottagit paketet,
+              förutsatt att produkten returneras oanvänd i oskadad
+              originalförpackning och i de fall där varan är plomberad skall
+              denna vara obruten. Vid retur betalar du själv returkostnaden.
+              Märk returen tydligt med MakeUpByS och ditt ordernummer och skicka
+              paketet till:
+            </Br>
+            <Br>
+              MakeUpByS AB
+              <br />
+              Öresten Ryd 4 <br /> 51191 Skene <br />
+            </Br>
+            <Br>
+              Vill du ha ett standardformulär för utövande av ångerrätten finns
+              det att ta del av på konsumentverkets hemsida,
+              www.konsumentverket.se Returhanteringen tar ca 1-3 veckor. Tänk på
+              att spara kvittot på din retur och skicka spårbart så att vi kan
+              ersätta dig för returen även i de fall paketet försvinner under
+              leveransen.
+            </Br>
+          </Flex>
+        </ContainerWithBorder>
       </MarginTopContainer>
     </AppShell>
   );
