89731522cddb83a10ee51ce19d1e853e67b9974b
totaldekadens <angelica.moberg@hotmail.com>
2023-01-13 08:08:39 +0100

    initial responsivity admin

--- components/admin/Options.tsx
+++ components/admin/Options.tsx
@@ -1,10 +1,13 @@
-import { Button, Flex } from "@mantine/core";
+import { Button, Flex, MediaQuery, Menu } from "@mantine/core";
+import { IconChevronDown } from "@tabler/icons";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Options = () => {
   const router = useRouter();
-
+  const [opened, setOpened] = useState(false);
+  const [currentPage, setCurrentPage] = useState<string>("Beställningar");
   const list = [
     { name: "Beställningar", link: "/admin", active: true },
     { name: "Produkter", link: "/admin/produkter", active: false },
@@ -20,23 +23,61 @@ const Options = () => {
       path.active = false;
     }
   });
+
+  useEffect(() => {
+    const getNumber = () => {
+      list.forEach((path) => {
+        if (router.pathname == path.link) {
+          setCurrentPage(path.name);
+        } else {
+        }
+      });
+    };
+    getNumber();
+  }, [opened]);
   return (
-    <Flex gap={5} mt={20}>
-      {list.map((button, index) => {
-        return (
-          <Link key={index} href={button.link}>
-            <Button
-              //size="xs"
-              w={100}
-              variant={button.active ? "filled" : "outline"}
-              color="brand.7"
-            >
-              {button.name}
-            </Button>
-          </Link>
-        );
-      })}
-    </Flex>
+    <>
+      <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+        <Flex gap={5} mt={20}>
+          {list.map((button, index) => {
+            return (
+              <Link key={index} href={button.link}>
+                <Button
+                  sx={{ minWidth: 100 }}
+                  variant={button.active ? "filled" : "outline"}
+                  color="brand.7"
+                >
+                  {button.name}
+                </Button>
+              </Link>
+            );
+          })}
+        </Flex>
+      </MediaQuery>
+      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+        <Flex gap={5} mt={20}>
+          <Menu opened={opened} onChange={setOpened}>
+            <Menu.Target>
+              <Button>
+                {currentPage}{" "}
+                <IconChevronDown style={{ marginLeft: 13 }} size={14} />
+              </Button>
+            </Menu.Target>
+            <Menu.Dropdown w={200}>
+              {list.map((button, index) => {
+                return (
+                  <Menu.Item color="brand.7">
+                    <Link key={index} href={button.link}>
+                      {button.name}
+                    </Link>
+                  </Menu.Item>
+                );
+              })}
+            </Menu.Dropdown>
+          </Menu>
+        </Flex>
+      </MediaQuery>
+    </>
   );
 };
 
