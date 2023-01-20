9c7dba106f95fb83b04936684805ff93bcd24ca3
totaldekadens <angelica.moberg@hotmail.com>
2023-01-20 17:47:10 +0100

    Able to search on product

--- components/SearchMobileFrontpage.tsx
+++ components/SearchMobileFrontpage.tsx
@@ -1,7 +1,65 @@
-import { Flex, Autocomplete, MediaQuery } from "@mantine/core";
+import {
+  Flex,
+  Autocomplete,
+  MediaQuery,
+  Group,
+  Avatar,
+  Text,
+} from "@mantine/core";
 import { IconSearch, IconX } from "@tabler/icons";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { forwardRef, useEffect, useState } from "react";
+import { ItemProps, PopulatedProduct } from "../utils/types";
 
 const SearchMobileFrontPage = () => {
+  const [value, setValue] = useState("");
+  const [data, setData] = useState<PopulatedProduct[]>([]);
+  const router = useRouter();
+
+  // Gets search result onchange
+  useEffect(() => {
+    if (value) {
+      const getSearchResult = async () => {
+        const request = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ value }),
+        };
+        let response = await fetch("/api/open/subproduct/search", request);
+        let result = await response.json();
+        setData(result.data);
+      };
+      getSearchResult();
+    }
+  }, [value]);
+
+  // Adjusts it to autocomplete
+  const newData = data.map((item) => ({ ...item, value: item.slug }));
+
+  const AutoCompleteItem = forwardRef<HTMLDivElement, ItemProps>(
+    (
+      { mainProduct, slug, value, title, images, ...others }: ItemProps,
+      ref
+    ) => (
+      <Link href={`/produkt/${slug}`}>
+        <div ref={ref} {...others}>
+          <Group noWrap>
+            <Avatar src={`/uploads/${images[0]}`} />
+            <div>
+              <Text>{title}</Text>
+              <Text size="xs" color="dimmed">
+                {mainProduct.price.$numberDecimal.toString() + " KR"}
+              </Text>
+            </div>
+          </Group>
+        </div>
+      </Link>
+    )
+  );
+
   return (
     <MediaQuery largerThan="xs" styles={{ display: "none" }}>
       <Flex
@@ -10,13 +68,19 @@ const SearchMobileFrontPage = () => {
         sx={{ width: "100%", paddingRight: "10px", paddingLeft: "10px" }}
       >
         <Autocomplete
+          value={value}
+          onChange={setValue}
+          data={newData}
+          onItemSubmit={(item) => {
+            router.push(`/produkt/${item.value}`);
+          }}
+          itemComponent={AutoCompleteItem}
           icon={<IconSearch color="white" size={15} stroke={3} />}
           rightSection={<IconX id="iconX" color="white" size={20} stroke={2} />}
           id="searchbar"
           color="white"
           placeholder="Sök..."
           variant="unstyled"
-          data={["Smink", "Läppstift"]}
           styles={{
             input: {
               color: "white",
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,58 @@
-import { Flex, Autocomplete } from "@mantine/core";
+import { Flex, Autocomplete, Group, Avatar, Text } from "@mantine/core";
 import { IconSearch, IconX } from "@tabler/icons";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { forwardRef, useEffect, useState } from "react";
+import { ItemProps, PopulatedProduct } from "../utils/types";
 
 const Searchbar = () => {
+  const [value, setValue] = useState("");
+  const [data, setData] = useState<PopulatedProduct[]>([]);
+  const router = useRouter();
+
+  // Gets search result onchange
+  useEffect(() => {
+    if (value) {
+      const getSearchResult = async () => {
+        const request = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ value }),
+        };
+        let response = await fetch("/api/open/subproduct/search", request);
+        let result = await response.json();
+        setData(result.data);
+      };
+      getSearchResult();
+    }
+  }, [value]);
+
+  // Adjusts it to autocomplete
+  const newData = data.map((item) => ({ ...item, value: item.slug }));
+
+  const AutoCompleteItem = forwardRef<HTMLDivElement, ItemProps>(
+    (
+      { mainProduct, slug, value, title, images, ...others }: ItemProps,
+      ref
+    ) => (
+      <Link href={`/produkt/${slug}`}>
+        <div ref={ref} {...others}>
+          <Group noWrap>
+            <Avatar src={`/uploads/${images[0]}`} />
+            <div>
+              <Text>{title}</Text>
+              <Text size="xs" color="dimmed">
+                {mainProduct.price.$numberDecimal.toString() + " KR"}
+              </Text>
+            </div>
+          </Group>
+        </div>
+      </Link>
+    )
+  );
+
   return (
     <Flex
       mb={8}
@@ -27,12 +78,18 @@ const Searchbar = () => {
       </Flex>
 
       <Autocomplete
+        onItemSubmit={(item) => {
+          router.push(`/produkt/${item.value}`);
+        }}
+        itemComponent={AutoCompleteItem}
         rightSection={<IconX id="iconX" color="white" size={20} stroke={2} />}
         miw={250}
         id="searchbar"
         placeholder="Sök..."
         variant="unstyled"
-        data={["Smink", "Läppstift"]}
+        value={value}
+        onChange={setValue}
+        data={newData}
         sx={(theme) => ({
           [theme.fn.smallerThan("lg")]: {
             minWidth: "180px",
@@ -54,7 +111,7 @@ const Searchbar = () => {
             "::placeholder": { color: "white" },
           },
         }}
-      ></Autocomplete>
+      />
     </Flex>
   );
 };
