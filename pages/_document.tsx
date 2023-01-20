017dfdcf75a0b1703c9306f5746dc3d59490df0c
totaldekadens <angelica.moberg@hotmail.com>
2023-01-20 14:03:30 +0100

    onödig kod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,26 +32,6 @@ export default class _Document extends Document {
       <Html style={{ scrollBehavior: "smooth" }}>
         <Head>
           <link rel="icon" href="/favicon.png" />
-          <meta
-            name="description"
-            content="Ett innovativt bokningssystem för dig som aktör i upplevelse-branschen som tillåter dig att ta emot bokningar direkt från din egen hemsida."
-          />
-          <title>
-            Bokningssystem för upplevelse-branschen - Adventure Hero
-          </title>
-
-          <meta
-            property="og:title"
-            content="Bokningssystemet för upplevelse-branschen - Adventure Hero"
-          />
-          <meta
-            property="og:description"
-            content="Ett innovativt bokningssystem för dig som aktör i upplevelse-branschen som tillåter dig att ta emot bokningar direkt från din egen hemsida."
-          />
-          <meta
-            property="og:url"
-            content="https://business.adventurehero.se/"
-          />
           <meta property="og:image" content="/logo.png" />
           <meta name="theme-color" content="#E5B4AD"></meta>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
