1ffecd2a49b1feba5ad84f6d73497a27c6717521
Jhoggen <jacob.hoggen@medieinstitutet.se>
2023-01-21 17:35:53 +0100

    fixat om header/nav på scroll
    #107 Nu är naven utan sök på scroll ner och scroll upp så kommer söken fram och när den når toppen så blir det transparant. kan va 1 litet problem så pushar nu för att kolla på mobilen

--- components/SearchMobileFrontpage.tsx
+++ components/SearchMobileFrontpage.tsx
@@ -1,13 +1,17 @@
 import { Flex, Autocomplete, MediaQuery } from "@mantine/core";
 import { IconSearch, IconX } from "@tabler/icons";
+import { hideContext } from "./context/HideProvider";
+import { useContext, useEffect, useRef, useState } from "react";
 
 const SearchMobileFrontPage = () => {
+  const { hide, setHide } = useContext(hideContext);
+ 
   return (
     <MediaQuery largerThan="xs" styles={{ display: "none" }}>
       <Flex
         align="flex-end"
         direction="row"
-        sx={{ width: "100%", paddingRight: "10px", paddingLeft: "10px" }}
+        sx={{ display: hide ? "none" : "flex" , width: "100%", paddingRight: "10px", paddingLeft: "10px" }}
       >
         <Autocomplete
           icon={<IconSearch color="white" size={15} stroke={3} />}
